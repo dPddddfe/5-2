fb04bb31662ae1d739bd63e2077ef7cfca3c4470
정예원학부생 <ysewon2004@handong.ac.kr>
2025-11-28 12:04:28 +0900

    main

--- src/components/Pages/UpdatePage.js
+++ src/components/Pages/UpdatePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Header from "../Header/Header";
 import { API_URL } from "../../api";
